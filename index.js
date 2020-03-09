d3bdc779bdc5480453b831ab7437a37d943452e6
Tumatauenga DIxon <tuma-dixon@live.nmit.ac.nz>
2020-03-10 12:50:21 +1300

    Math

---
+++ index.js
@@ -0,0 +1,13 @@
+let x = 10
+let y = 15
+let z = 30
+
+let sumOne = Math.pow(x, 3); // The math.pow function works out variable x to the power of 3 (x cubed)
+let sumTwo = Math.pow(y, 3);
+let sumThree = Math.pow(z, 3);
+
+console.log(sumOne)
+console.log(sumTwo)
+console.log(sumThree)
+
+console.log(sumOne + sumTwo + sumThree) // This is the sum of all cubed numbers added together
